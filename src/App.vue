ef9fa33a3dd2fd34e96dfea0bc5a5a1b110ec95c
daisuke <dsaito85a@gmail.com>
2020-09-08 19:56:29 +0900

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,17 @@
     <navigation />
     <hero />
     <subnavigation />
-    <router-view/>
+    <div class="page__content">
+      <router-view/>
+    </div>
+    <div class="page__footer">
+      <a class="page__source-link" :href="$route.meta.githubUrl" target="_blank">
+        <icon name="code" />
+        <span>
+          Show Code
+        </span>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -11,12 +21,16 @@
 import Navigation from '@/components/Navigation/index.vue';
 import Hero from '@/components/Hero/index.vue';
 import Subnavigation from '@/components/Subnavigation/index.vue';
+import Icon from '@/components/Icon/index.vue';
 
 export default {
   components: {
     Navigation,
     Hero,
     Subnavigation,
+    Icon,
   },
 };
 </script>
+
+<style lang="scss" src="./style.scss"></style>
