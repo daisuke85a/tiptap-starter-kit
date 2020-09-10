ae9e1662b9d82f22b3c8047394ebc37343e74bd1
daisuke <dsaito85a@gmail.com>
2020-09-10 16:03:37 +0900

    feat: add MenuBubble, FloatingMenu, Links

--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,30 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    component: () => import(/* webpackChunkName: "basic" */ '../views/Basic/Index.vue'),
+    component: () => import(/* webpackChunkName: "basic" */ '../views/Basic/index.vue'),
     meta: {
-      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/Basic/Index.vue',
+      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/Basic/index.vue',
+    },
+  },
+  {
+    path: '/menu-bubble',
+    component: () => import(/* webpackChunkName: "menu-bubble" */ '../views/MenuBubble/index.vue'),
+    meta: {
+      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/MenuBubble/index.vue',
+    },
+  },
+  {
+    path: '/floating-menu',
+    component: () => import(/* webpackChunkName: "floating-menu" */ '../views/FloatingMenu/index.vue'),
+    meta: {
+      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/MenuBubble/FloatingMenu/index.vue',
+    },
+  },
+  {
+    path: '/links',
+    component: () => import(/* webpackChunkName: "links" */ '../views/Links/index.vue'),
+    meta: {
+      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/MenuBubble/Links/index.vue',
     },
   },
 ];
