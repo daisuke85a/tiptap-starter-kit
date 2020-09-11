3f46ee56ebf32ccfca1fe0a1fa14aa3600be86ab
daisuke <dsaito85a@gmail.com>
2020-09-11 13:26:27 +0900

    feat:  ADD HidingMenuBar,Images,Tables,TodoList

--- src/router/index.js
+++ src/router/index.js
@@ -22,16 +22,45 @@ const routes = [
     path: '/floating-menu',
     component: () => import(/* webpackChunkName: "floating-menu" */ '../views/FloatingMenu/index.vue'),
     meta: {
-      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/MenuBubble/FloatingMenu/index.vue',
+      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/FloatingMenu/index.vue',
     },
   },
   {
     path: '/links',
     component: () => import(/* webpackChunkName: "links" */ '../views/Links/index.vue'),
     meta: {
-      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/MenuBubble/Links/index.vue',
+      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/Links/index.vue',
     },
   },
+  {
+    path: '/images',
+    component: () => import(/* webpackChunkName: "Images" */ '../views/Images/index.vue'),
+    meta: {
+      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/Images/index.vue',
+    },
+  },
+  {
+    path: '/hiding-menu-bar',
+    component: () => import(/* webpackChunkName: "hiding-menu-bar" */ '../views/HidingMenuBar/index.vue'),
+    meta: {
+      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/HidingMenuBar/index.vue',
+    },
+  },
+  {
+    path: '/tables',
+    component: () => import(/* webpackChunkName: "tables" */ '../views/Tables/index.vue'),
+    meta: {
+      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/Tables/index.vue',
+    },
+  },
+  {
+    path: '/todo-list',
+    component: () => import(/* webpackChunkName: "tables" */ '../views/TodoList/index.vue'),
+    meta: {
+      githubUrl: 'https://github.com/daisuke85a/tiptap-starter-kit/blob/master/src/views/TodoList/index.vue',
+    },
+  },
+
 ];
 
 const router = new VueRouter({
